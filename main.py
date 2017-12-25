2b6bc372968df315212f83bf57c44433777ef0d4
komissarov <komissar.off.andrey@gmail.com>
2017-12-25 21:07:21 +0300

    small fix

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import sys
 
+import generetic
+
 def main(argv):
-	pass
+	if len(argv) > 0:
+		filename = argv[1]
+	else:
+		filename = 'input.csv'
+
+	try:
+		data = open(filename).read()
+	except Exception as e:
+		print('Exception while reading file: %s'%e)
+		sys.exit(1)
 
 if __name__ == "__main__":
 	main(sys.argv)
