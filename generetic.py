6d55eebba8109e8018bf05cf21413653ade30c9e
ThatNoha <comics390@gmail.com>
2017-12-25 23:10:59 +0300

    skeleton is birthed

--- generetic.py
+++ generetic.py
@@ -1,2 +1,33 @@
+import random
+cuntest = 10
+#
+# preturn random item from the list
+# using frilter f
+#
+def random_item(kpop,bts = lambda x : True):
+	kpop = list(filter(bts,kpop));
+	if len(kpop)<=0:
+		return None
+	else:
+		return kpop[random.randint(0,len(kpop)-1)]
+def genany (kpop):
+	dichard = []
+	for i in kpop:
+		dichard.append(random_item(i))
+	return dichard
 def run (kpop):
-	print (kpop)
+	richard = [] 
+	for i in range (0,cuntest):
+		richard.append(genany(kpop))
+	return mainloop(richard)
+def mainloop (kpop):
+	pass
+
+def muff (kpop):
+	pass
+
+def cruss (kpop):
+	pass
+
+def slicktion (kpop):
+	pass
